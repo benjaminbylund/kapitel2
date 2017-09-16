c143558c875de195562c6dc851563b3149ff2e58
Benjamin Bylund <bybe16@nti.umea>
2017-09-16 09:42:28 +0200

    string s4

--- StringVariabler.java
+++ StringVariabler.java
@@ -2,13 +2,13 @@ import javax.swing.*;
 
 public class StringVariabler {
 	public static void main(String[] args) {
-		String s1, s2, s3;
+		String s1, s2, s3, s4;
 
 		s1 = "Benjamin";
 		s2 = "Teg";
 		s3 = "112";	
-		s2 = s1 + "\n" + s2 + "\n" + s3;
+		s4 = s1 + "\n" + s2 + "\n" + s3;
 
-			JOptionPane.showMessageDialog(null, s2);
+			JOptionPane.showMessageDialog(null, s4);
 	}	
 }

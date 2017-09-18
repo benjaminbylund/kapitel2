c818da04a31caa85d936f06e22845c21614f9f76
Benjamin Bylund <bybe16@nti.umea>
2017-09-18 10:48:34 +0200

    Uppgift 2.6 och 2.7

--- CharVariabler.java
+++ CharVariabler.java
@@ -2,13 +2,13 @@ import javax.swing.*;
 
 public class CharVariabler{	
 	public static void main(String[] args) {
-		String c1, c2, c3;
+		char c1, c2, c3;
 
-		c1 = "\u00D8";	
-		c2 = "\u00F8";
-		c3 = "\u03C0";
-		c2 = c1 + " " + c2 + " " + c3;
-			JOptionPane.showMessageDialog(null, c2);
+		c1 = '\u00D8';	
+		c2 = '\u00F8';
+		c3 = '\u03C0';
+
+			JOptionPane.showMessageDialog(null,""+c1+c2+c3);
 
 	}
 }
